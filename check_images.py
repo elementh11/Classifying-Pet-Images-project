b5ecaf66719373ba94ff8001a62c08e5a5256bdc
Adrian Popescu <elementh11@gmail.com>
2019-10-01 10:32:54 -0400

    Update check_images.py

--- check_images.py
+++ check_images.py
@@ -5,7 +5,7 @@
 # TODO 0: Add your information below for Programmer & Date Created.                                                                             
 # PROGRAMMER: Adrian P.
 # DATE CREATED: 2/19/2019                                 
-# REVISED DATE: 
+# REVISED DATE: 10/01/2019
 # PURPOSE: Classifies pet images using a pretrained CNN model, compares these
 #          classifications to the true identity of the pets in the images, and
 #          summarizes how well the CNN performed on the image classification task. 
