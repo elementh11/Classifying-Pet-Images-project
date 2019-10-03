4231ced80af5ed2bdca3f43e6d2cbfbf0a7a4f82
Adrian Popescu <elementh11@gmail.com>
2019-10-03 09:23:36 -0400

    Update classify_images.py

--- classify_images.py
+++ classify_images.py
@@ -5,22 +5,6 @@
 # PROGRAMMER: Adrian P.
 # DATE CREATED: 2/21/2019                                
 # REVISED DATE: 10/3/2019
-# PURPOSE: Create a function classify_images that uses the classifier function 
-#          to create the classifier labels and then compares the classifier 
-#          labels to the pet image labels. This function inputs:
-#            -The Image Folder as image_dir within classify_images and function 
-#             and as in_arg.dir for function call within main. 
-#            -The results dictionary as results_dic within classify_images 
-#             function and results for the functin call within main.
-#            -The CNN model architecture as model wihtin classify_images function
-#             and in_arg.arch for the function call within main. 
-#           This function uses the extend function to add items to the list 
-#           that's the 'value' of the results dictionary. You will be adding the
-#           classifier label as the item at index 1 of the list and the comparison 
-#           of the pet and classifier labels as the item at index 2 of the list.
-#
-##
-# Imports classifier function for using CNN to classify images 
 from classifier import classifier 
 
 # TODO 3: Define classify_images function below, specifically replace the None
