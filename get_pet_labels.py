dc4c79e15a046ef630efaae0f4dee4b2bca020aa
Adrian Popescu <elementh11@gmail.com>
2019-10-08 09:39:57 -0400

    Update get_pet_labels.py

--- get_pet_labels.py
+++ get_pet_labels.py
@@ -29,9 +29,6 @@ def get_pet_labels(image_dir):
     of the image files. These pet image labels are used to check the accuracy 
     of the labels that are returned by the classifier function, since the 
     filenames of the images contain the true identity of the pet in the image.
-    Be sure to format the pet labels so that they are in all lower case letters
-    and with leading and trailing whitespace characters stripped from them.
-    (ex. filename = 'Boston_terrier_02259.jpg' Pet label = 'boston terrier')
     Parameters:
      image_dir - The (full) path to the folder of images that are to be
                  classified by the classifier function (string)
