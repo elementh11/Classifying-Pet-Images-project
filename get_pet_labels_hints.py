1cdcf2ea881891464ae7dcb0e97957c05e9e9887
Adrian Popescu <elementh11@gmail.com>
2019-10-09 10:32:53 -0400

    Update get_pet_labels_hints.py

--- get_pet_labels_hints.py
+++ get_pet_labels_hints.py
@@ -20,11 +20,7 @@
 # Imports python modules
 from os import listdir
 
-# TODO 2: EDIT and ADD code BELOW to do the following that's stated in the 
-#       comments below that start with "TODO: 2" for the get_pet_labels function 
-#       Please be certain to replace None in the return statement with 
-#       results_dic dictionary that you create with this function
-# 
+ 
 def get_pet_labels(image_dir):
     """
     Creates a dictionary of pet labels (results_dic) based upon the filenames 
