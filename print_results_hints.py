0bf474ad60365fa3a7afa3dc0e1d49f27b07be49
Adrian Popescu <elementh11@gmail.com>
2019-10-10 09:26:59 -0400

    Update print_results_hints.py

--- print_results_hints.py
+++ print_results_hints.py
@@ -11,19 +11,6 @@
 #          allow the user to be able to print out cases of misclassified
 #          dogs and cases of misclassified breeds of dog using the Results 
 #          dictionary (results_dic).  
-#         This function inputs:
-#            -The results dictionary as results_dic within print_results 
-#             function and results for the function call within main.
-#            -The results statistics dictionary as results_stats_dic within 
-#             print_results function and results_stats for the function call within main.
-#            -The CNN model architecture as model wihtin print_results function
-#             and in_arg.arch for the function call within main. 
-#            -Prints Incorrectly Classified Dogs as print_incorrect_dogs within
-#             print_results function and set as either boolean value True or 
-#             False in the function call within main (defaults to False)
-#            -Prints Incorrectly Classified Breeds as print_incorrect_breed within
-#             print_results function and set as either boolean value True or 
-#             False in the function call within main (defaults to False)
 #         This function does not output anything other than printing a summary
 #         of the final results.
 # 
