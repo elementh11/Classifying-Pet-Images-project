a9e1b20c0c383290be4b4d079b7f454f6e5cd115
Adrian Popescu <elementh11@gmail.com>
2019-10-14 10:06:43 -0400

    Update test_classifier.py

--- test_classifier.py
+++ test_classifier.py
@@ -25,10 +25,6 @@ test_image="pet_images/Collie_03797.jpg"
 #      'vgg', 'alexnet', 'resnet'  
 model = "vgg"
 
-# Demonstrates classifier() functions usage
-# NOTE: image_classication is a text string - It contains mixed case(both lower
-# and upper case letter) image labels that can be separated by commas when a 
-# label has more than one word that can describe it.
 image_classification = classifier(test_image, model)
 
 # prints result from running classifier() function
