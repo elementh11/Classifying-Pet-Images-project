092ab3dad56457e64db40eb23d18589f8a1984ae
Adrian Popescu <elementh11@gmail.com>
2019-10-14 10:05:15 -0400

    Update run_models_batch_uploaded.sh

--- run_models_batch_uploaded.sh
+++ run_models_batch_uploaded.sh
@@ -3,7 +3,7 @@
 #                                                                             
 # PROGRAMMER: Jennifer S.
 # DATE CREATED: 02/08/2018                                  
-# REVISED DATE: 02/27/2018  - 
+# REVISED DATE: 10/14/2019  - 
 # PURPOSE: Runs all three models to test which provides 'best' solution on the Uploaded Images.
 #          Please note output from each run has been piped into a text file.
 #
