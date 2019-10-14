32dcc55f582388311d38220dd62072ea74e3c4ab
Adrian Popescu <elementh11@gmail.com>
2019-10-14 10:07:12 -0400

    Update test_classifier.py

--- test_classifier.py
+++ test_classifier.py
@@ -20,9 +20,6 @@ from classifier import classifier
 # Defines a dog test image from pet_images folder
 test_image="pet_images/Collie_03797.jpg"
 
-# Defines a model architecture to be used for classification
-# NOTE: this function only works for model architectures: 
-#      'vgg', 'alexnet', 'resnet'  
 model = "vgg"
 
 image_classification = classifier(test_image, model)
